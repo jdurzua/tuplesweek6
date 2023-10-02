288764c14daf707c4ef97d610f9cdc9824b0660e
jdurzua <142839630+jdurzua@users.noreply.github.com>
2023-10-02 19:45:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+print("Herminio, Josue, Emmanuel")
+# Tuples Practice #1
+# Use a tuple method to count the number of times the value 2 appears in the following tuple,  and display the result (integer) on the screen:
+tuple1 = (1, 2, 3, 4, 5, 6, 7, 8)
+
+# Tuples Practice #2
+# Convert the following tuple to a list, and store it in a variable called my_list.
+
+tuple2 = {1, 2, 3, 2, 3, 1, 3, 2}
+my_list = list(tuple2)
+print(my_list)
+
+# Tuples Practice #3
+# Extract the elements of the following tuple into four variables: a, b, c, d
+
+tuple3 = (1, 2, 3, 4)
+a, b, c, d = tuple3
+print(a, b, c, d)
